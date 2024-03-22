180b818363798c86d68d59b5320fdc3cdeb4c7c3
imran980 <96299174+imran980@users.noreply.github.com>
2024-03-22 10:05:01 +0500

    Update clip_wrapper.py

--- scripts/clip_wrapper.py
+++ scripts/clip_wrapper.py
@@ -125,9 +125,3 @@ class ContrastiveCLIPWrapper(ClipWrapper):
         super().__init__(model)
         self.vision_model = ContrastiveLearner(self.vision_model, temperature)
         self.text_model = ContrastiveLearner(self.text_model, temperature)
-
-    def get_image_features(self, x, output_hidden_states=False, emb_input=False):
-        return self.vision_model(x)
-
-    def get_text_features(self, x, output_hidden_states=False, emb_input=False):
-        return self.text_model(x)
