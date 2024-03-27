11f815effe2a0742efc1a4200766f50395eba59b
imran980 <96299174+imran980@users.noreply.github.com>
2024-03-27 10:52:36 +0500

    Update cross_attention.py

--- scripts/cross_attention.py
+++ scripts/cross_attention.py
@@ -2,12 +2,25 @@ import torch
 import torch.nn as nn
 
 class CrossAttentionLayer(nn.Module):
-    def __init__(self, dim_model=512, num_heads=8):
+    def __init__(self, dim):
         super(CrossAttentionLayer, self).__init__()
-        self.dim_model = dim_model
-        self.num_heads = num_heads
-        self.cross_attention = nn.MultiheadAttention(dim_model, num_heads, batch_first=True)
+        self.dim = dim
+        self.query_projection = nn.Linear(dim, dim)
+        self.key_projection = nn.Linear(dim, dim)
+        self.value_projection = nn.Linear(dim, dim)
+        self.output_projection = nn.Linear(dim, dim)
 
     def forward(self, vision_repr, text_repr):
-        cross_attended_repr, _ = self.cross_attention(vision_repr, text_repr, text_repr)
+        batch_size = vision_repr.size(0)
+        query = self.query_projection(vision_repr).view(batch_size, -1, self.dim)
+        key = self.key_projection(text_repr).view(batch_size, -1, self.dim)
+        value = self.value_projection(text_repr).view(batch_size, -1, self.dim)
+
+        attention_scores = torch.bmm(query, key.transpose(1, 2))
+        attention_scores = attention_scores / (self.dim ** 0.5)
+        attention_probs = nn.Softmax(dim=-1)(attention_scores)
+
+        cross_attended_repr = torch.bmm(attention_probs, value)
+        cross_attended_repr = self.output_projection(cross_attended_repr.view(batch_size, -1, self.dim))
+
         return cross_attended_repr
