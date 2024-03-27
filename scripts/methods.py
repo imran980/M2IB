6ba4d4546d7e28f74286ad6839afac91aa516f5a
imran980 <96299174+imran980@users.noreply.github.com>
2024-03-28 02:42:03 +0500

    Update methods.py

--- scripts/methods.py
+++ scripts/methods.py
@@ -11,7 +11,10 @@ from transformers import CLIPProcessor, CLIPModel, CLIPTokenizerFast
 
 # Feature Map is the output of a certain layer given X
 def extract_feature_map(model, layer_idx, x, is_text=False):
+    print("anything-----------------------------------")
+    print("printing test--------------------", dir(model))
     print("third vision model--------------------:", model.vision_model)
+    
     with torch.no_grad():
         if is_text:
             print("first vision model--------------------:", model.vision_model)
