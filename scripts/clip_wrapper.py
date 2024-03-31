a239c81cdf25575bf4543a421b4c4e5e7020e414
imran980 <96299174+imran980@users.noreply.github.com>
2024-04-01 02:28:15 +0500

    Update clip_wrapper.py

--- scripts/clip_wrapper.py
+++ scripts/clip_wrapper.py
@@ -117,6 +117,7 @@ class text_encoder_wrapper(nn.Module):
 class ClipWrapper(nn.Module):
     def __init__(self, model):
         super().__init__()
+        print("clipwrapper------------------:")
         print("Model object--------------------:", model)
         print("Model visual--------------------:", model.visual)
         self.vision_model = image_encoder_wrapper(copy.deepcopy(model.visual), model.dtype).to(device)
