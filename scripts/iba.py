5059bc2a8f96545d43e5f39469c1534bdac96948
imran980 <96299174+imran980@users.noreply.github.com>
2024-04-14 02:24:09 +0500

    Update iba.py

--- scripts/iba.py
+++ scripts/iba.py
@@ -168,31 +168,25 @@ class IBAInterpreter:
         return normalize(saliency)
 
     def _run_text_training(self, text_t, image_t):
-        text_repr = self.model.get_text_features(text_t)
-        image_repr = self.model.get_image_features(image_t)
-        cross_attended_text, cross_attended_image = self.cross_attention(image_repr, text_repr)
+        cross_attended_text, cross_attended_image = self.cross_attention(image_t, text_t)
 
         replace_layer(self.model.text_model, self.original_layer, self.sequential)
         loss_c, loss_f, loss_t = self._train_bottleneck(cross_attended_text, cross_attended_image)
         replace_layer(self.model.text_model, self.sequential, self.original_layer)
         return self.bottleneck.buffer_capacity.mean(axis=0), loss_c, loss_f, loss_t
-	    
+
     def _run_vision_training(self, text_t, image_t):
-        text_repr = self.model.get_text_features(text_t)
-        image_repr = self.model.get_image_features(image_t)
         print("run_vision_training-----------------------:")
         print("text_repr-----------------------:",text_repr)
         print("image_repr-----------------------:",image_repr)
-        cross_attended_vision, cross_attended_image = self.cross_attention(image_repr, text_repr)
+        cross_attended_vision, cross_attended_image = self.cross_attention(image_t, text_t)
         print("cross_attended_vision-----------------------:",cross_attended_vision)
         print("cross_attended_image-----------------------:",cross_attended_image)
-    
         replace_layer(self.model.vision_model, self.original_layer, self.sequential)
         loss_c, loss_f, loss_t = self._train_bottleneck(cross_attended_vision, cross_attended_image)
         print("loss_c-----------------------:",loss_c)
         print("loss_f-----------------------:",loss_f)
         print("loss_t-----------------------:",loss_t)
-
         replace_layer(self.model.vision_model, self.sequential, self.original_layer)
         return self.bottleneck.buffer_capacity.mean(axis=0), loss_c, loss_f, loss_t
 
