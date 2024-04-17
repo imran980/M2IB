e46522589123cc138b836b8a837f77f7efcfaeca
imran980 <96299174+imran980@users.noreply.github.com>
2024-04-17 20:43:20 +0500

    Update utils.py

--- scripts/utils.py
+++ scripts/utils.py
@@ -14,13 +14,13 @@ def normalize(x):
     return (x - x.min()) / (x.max() - x.min())
 
 class mySequential(nn.Sequential):
-    def forward(self, _input, **kwargs):
+    def forward(self, *input, **kwargs):
         for module in self._modules.values():
-            if isinstance(_input, tuple):
-                _input = module(*_input, **kwargs)
+            if type(input) == tuple:
+                input = module(*input)
             else:
-                _input = module(_input, **kwargs)
-        return _input
+                input = module(input)
+        return input
 
 def replace_layer(model: nn.Module, target: nn.Module, replacement: nn.Module):
     """
