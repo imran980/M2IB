000962d1a7c9e7f924a3c107413964c19073a693
imran980 <96299174+imran980@users.noreply.github.com>
2024-04-17 22:41:06 +0500

    Update cross_attention.py

--- scripts/cross_attention.py
+++ scripts/cross_attention.py
@@ -11,27 +11,21 @@ class CrossAttentionLayer(nn.Module):
         self.softmax = nn.Softmax(dim=-1)
 
     def forward(self, inputs):
+       
         print("cross attention inputs shape---------------", inputs.shape)
         print("cross attention inputs---------------", inputs)
-        
-        vision_repr, text_repr = inputs
-        # ... (the rest of your code for the CrossAttentionLayer)
-        # Calculate attention scores
-        print("vision repr crossattentionlayer------------------:", vision_repr)
-        print("text repr crossattentionlayer------------------:", text_repr)
+        batch_size, sequence_length, embedding_size = inputs.size()
+        vision_repr = inputs[:, :, :embedding_size // 2]
+        text_repr = inputs[:, :, embedding_size // 2:]
+
         query = self.query(vision_repr)
-        print("query crossattentionlayer------------------:", query)
         key = self.key(text_repr)
-        print("key crossattentionlayer------------------:", key)
-        attention_scores = torch.matmul(query, key.transpose(-2, -1)) / torch.sqrt(torch.tensor(self.dim_model))
-        print("attention_scores crossattentionlayer------------------:", attention_scores)
-        # Compute attention weights
+        attention_scores = torch.matmul(query, key.transpose(-2, -1)) / torch.sqrt(torch.tensor(self.dim_model, dtype=torch.float))
+        
         attention_weights = self.softmax(attention_scores)
-        print("attention_weights crossattentionlayer------------------:", attention_weights)
-        # Calculate cross-attended representations
+        
         value = self.value(text_repr)
         cross_attended_vision = torch.matmul(attention_weights, value)
         cross_attended_text = torch.matmul(attention_weights.transpose(-2, -1), vision_repr)
-        print("cross_attended_vision crossattentionlayer------------------:", cross_attended_vision)
-        print("cross_attended_text crossattentionlayer------------------:", cross_attended_text)
+        
         return cross_attended_vision, cross_attended_text
