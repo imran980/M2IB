8aa231604d376f93526641aed5c44b3c783ac292
imran980 <96299174+imran980@users.noreply.github.com>
2024-04-19 08:27:20 +0500

    Update cross_attention.py

--- scripts/cross_attention.py
+++ scripts/cross_attention.py
@@ -9,27 +9,21 @@ class CrossAttentionLayer(nn.Module):
         self.key = nn.Linear(dim_model, dim_model)
         self.value = nn.Linear(dim_model, dim_model)
         self.softmax = nn.Softmax(dim=-1)
-
     def forward(self, inputs):
-       
+        batch_size, sequence_length, embedding_size = inputs.size()
         print("cross attention inputs shape---------------", inputs.shape)
         print("cross attention inputs---------------", inputs)
-        batch_size, sequence_length, embedding_size = inputs.size()
-        vision_repr = inputs[:, :, :embedding_size // 2]
-        print("cross attention vision_repr shape---------------", vision_repr.shape)
-        text_repr = inputs[:, :, embedding_size // 2:]
-        print("cross attention text_repr shape---------------", text_repr.shape)
-        query = self.query(vision_repr)
+        query = self.query(inputs)
         print("cross attention query---------------", query)
-        key = self.key(text_repr)
+        key = self.key(inputs)
         print("cross attention key---------------", key)
         attention_scores = torch.matmul(query, key.transpose(-2, -1)) / torch.sqrt(torch.tensor(self.dim_model, dtype=torch.float))
-        print("cross attention attention_scores---------------", attention_scores)
+
         attention_weights = self.softmax(attention_scores)
-        print("cross attention attention_weights---------------", attention_weights)
-        value = self.value(text_repr)
+        print("cross attention attention_scores---------------", attention_scores)
+        value = self.value(inputs)
         print("cross attention inputs---------------", value)
         cross_attended_vision = torch.matmul(attention_weights, value)
-        cross_attended_text = torch.matmul(attention_weights.transpose(-2, -1), vision_repr)
+        cross_attended_text = torch.matmul(attention_weights.transpose(-2, -1), inputs)
         
         return cross_attended_vision, cross_attended_text
