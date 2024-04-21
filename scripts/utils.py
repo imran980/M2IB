f828d7292e6ea87bdb254333cee094a891c21e7b
imran980 <96299174+imran980@users.noreply.github.com>
2024-04-21 22:11:22 +0500

    Update utils.py

--- scripts/utils.py
+++ scripts/utils.py
@@ -64,9 +64,9 @@ def replace_layer(model: nn.Module, target: nn.Module, replacement: nn.Module):
             else:
                 # Call the original forward method
                 return self.module(*args, **kwargs)
-    else:
-        # Call the original forward method directly
-        return self.forward(*args, **kwargs)
+        else:
+            # Call the original forward method directly
+            return self.forward(*args, **kwargs)
 
     if not replace_in(model, target, replacement):
         raise RuntimeError("Cannot substitute layer: Layer of type " + target.__class__.__name__ + " is not a child of given parent of type " + model.__class__.__name__)
