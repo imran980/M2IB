1aac3730af54ef968339258a32cd2362909ee5bc
imran980 <96299174+imran980@users.noreply.github.com>
2024-06-12 08:08:13 +0500

    Update iba.py

--- scripts/iba.py
+++ scripts/iba.py
@@ -194,7 +194,7 @@ class IBAInterpreter:
             optimizer.step(closure=None)
         return loss_c, loss_f, loss_t 
 
-    def calc_loss(self, outputs, labels, temperature=0.7):
+    def calc_loss(self, outputs, labels, temperature=0.4):
         """
         Calculate the combined loss expression for optimization of lambda
         Inputs:
